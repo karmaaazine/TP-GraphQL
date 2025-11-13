ce674ca1a7d3aef67f8cc8d0dea8ce056c584dcb
richdaleintern06 <richdaleintern06@gmail.com>
2025-11-13 10:48:53 +0100

    first commit

---
+++ src/main/java/com/example/srinboot/SrinbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.srinboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrinbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrinbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/srinboot/SrinbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srinboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrinbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
